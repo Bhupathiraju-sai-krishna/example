b47e9959ccd5cd3e7c12148e74acd29571a1d5bc
DELL <DELL@saivarma>
2021-06-04 20:02:03 +0530

    added subtraction function

--- Test/src/Adding.java
+++ Test/src/Adding.java
@@ -2,10 +2,12 @@
 public class Adding {
 
 	public static void main(String[] args) {
-		int a =10;
-		int b=20;
-		int c= a+b;
+		int a = 10;
+		int b = 20;
+		int c = a + b;
 		System.out.println(c);
+		int d = b - a;
+		System.out.println(d);
 
 	}
 
